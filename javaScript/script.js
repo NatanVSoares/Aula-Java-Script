802ba685fe15e336d05798685647d41cd07de7a4
NatanVSoares <natan.vitor@icloud.com>
2025-03-19 21:59:54 -0300

    Aula sobre Dom

---
+++ javaScript/script.js
@@ -0,0 +1,40 @@
+function mudarCorTexto(){
+    
+    const paragrafo = document.getElementById('paragrafo-principal')
+
+    paragrafo.classList.add('vermelho')
+    
+}
+
+function adicionarBorda(){
+
+    const paragrafo = document.getElementById('paragrafo-principal')
+    paragrafo.classList.add('borda')
+    
+}
+
+function mudarFundo(){
+    const mudandoFundo = document.getElementById('area')
+    mudandoFundo.classList.add('amarelo')
+
+}
+
+function adicionarParagrafo(){
+    const primeiroParagrafo = document.getElementById('frase')
+    primeiroParagrafo.classList.add(primeiro-paragrafo)
+}
+
+
+
+
+/*
+
+   Passo 1: Criar botão 
+   Passo 2: Colocar o botão Onclick
+   Passo 3:criar função no js
+   Passo 4:Identificar o elemento que eu quero mexer (paragrafo)
+   Passo: Adicionar classe
+
+*/
+
+
