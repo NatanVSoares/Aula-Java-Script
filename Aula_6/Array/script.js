901978be91d84b198c5d51390fe8376460b5e351
NatanVSoares <natan.vitor@icloud.com>
2025-04-09 21:46:14 -0300

    Arrays

---
+++ Aula_6/Array/script.js
@@ -0,0 +1,46 @@
+//Exercício_01:
+
+
+console.log("Exe_01")
+
+let cores = ["vermelho","azul","verde","amarelo"]
+
+let  CoresQuantidades = cores.length
+console.log(`A quantidades de cores são: ${CoresQuantidades}`)
+
+
+console.log("Exe_02")
+let animais=["gato","cachorro","elefante","leão","tigre"]
+
+console.log(animais.length)
+
+
+
+console.log("Exe_03")
+let frutas=["maça","banana","uva"]
+
+frutas.pop()
+frutas.push("laranja")
+
+
+console.log(frutas)
+
+
+
+console.log("Exe_04")
+let times =["Flamengo","Palmeiras","São paulo","Grêmio","Cruzeiro"]
+let timesPaoulistas=["São Paulo","Palmeiras"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
